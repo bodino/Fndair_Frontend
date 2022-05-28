3d88ed897007e1fecd90d033b00fda4066ffdd5c
bodino <a.bifulco23@gmail.com>
2022-05-28 15:46:15 -0700

    test

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const crypto = require('crypto');
 const cors = require('cors')
 const fs = require('fs').promises;
 const free = require('./PaymentListener.js');
-var PNG = require('png-js');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(cors())
 const mongoose = require("mongoose");
 //mongodb stuff
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const Wallet = require('./Backend/Backend/Wallet.js');
+const User = require('./Backend/Backend/User.js');
+const Protocol = require('./Backend/Backend/Protocol.js');
 const uri = "";
 
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 mongoose.connect('mongodb+srv://bodo:pkPau37eVc3HHtNX@fndair.6qw8v.mongodb.net/?retryWrites=true&w=majority');
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
  
 db.once('open', function() {
@@ -42,46 +45,8 @@ const kittySchema = new mongoose.Schema({
 
 //   silence.save();
 
-console.log(Frodo.find().then(result => console.log(result)));
+// console.log(Frodo.find().then(result => console.log(result)));
 
-var Projects = {
-    Optimism: {
-        Website: "https://app.optimism.io/governance",
-        Icon: "",
-        Claimable: "No",
-        EstimatedClaimDate: "Q2",
-        Decimals: "18", 
-    },
-    Hop: {
-        Website: "https://app.hop.exchange/#/airdrop/preview",
-        Icon: "",
-        Claimable: "No",
-        EstimatedClaimDate: "Q2",
-        Decimals: "16", 
-    },
-    ENS: {
-        Website: "https://app.hop.exchange/#/airdrop/preview",
-        Icon: "",
-        Claimable: "Missed",
-        EstimatedClaimDate: "",
-        Decimals: "18", 
-    },
-    Ribbon: {
-        Website: "https://app.hop.exchange/#/airdrop/preview",
-        Icon: "",
-        Claimable: "Yes",
-        EstimatedClaimDate: "",
-        Decimals: "18", 
-    }
-}
-var Addresses = {
-    // "0x6E9540950B46c35C7C419e57eF6dc6F946B95338": {
-    //     Optimism: '',
-    //     Hop: '100324123412351234534',
-    //     ENS: '',
-    //     Ribbon: '',
-    // }
-};
 const getHashedPassword = (password) => {
     const sha256 = crypto.createHash('sha256');
     const hash = sha256.update(password).digest('base64');
@@ -102,45 +67,45 @@ app.listen(3001, function (){
 })
 
 
-app.post('/NewAirdrop', function(req,res){
+// app.post('/NewAirdrop', function(req,res){
 
-   const body = req.body;
-   var test = body.NewAirDrop.Password.toString()
-   body.NewAirDrop.Data = require("/Users/alessandrobifulco/Downloads/finalDistribution.json");
-   body.NewAirDrop.Name = "Hop"
-//    console.log(body.NewAirDrop.Data);
+//    const body = req.body;
+//    var test = body.NewAirDrop.Password.toString()
+//    body.NewAirDrop.Data = require("/Users/alessandrobifulco/Downloads/finalDistribution.json");
+//    body.NewAirDrop.Name = "Hop"
+// //    console.log(body.NewAirDrop.Data);
 
-   for (const key in body.NewAirDrop.Data ) {
+//    for (const key in body.NewAirDrop.Data ) {
 
-    if (body.NewAirDrop.Data .hasOwnProperty(key)) {
+//     if (body.NewAirDrop.Data .hasOwnProperty(key)) {
 
-      var address = `${key}`;
-    //   console.log(addressess[address]) ;
-      if (Addresses[address] == undefined){
-        // console.log(body.NewAirDrop.Name)
-        const silence = new Frodo({address: address,
-            numberoftokens: body.NewAirDrop.Data[address].totalTokens,
-       });
-       silence.save();
-        Addresses[address] = {
-            [body.NewAirDrop.Name]: body.NewAirDrop.Data[address].totalTokens,
+//       var address = `${key}`;
+//     //   console.log(addressess[address]) ;
+//       if (Addresses[address] == undefined){
+//         // console.log(body.NewAirDrop.Name)
+//         const silence = new Frodo({address: address,
+//             numberoftokens: body.NewAirDrop.Data[address].totalTokens,
+//        });
+//        silence.save();
+//         Addresses[address] = {
+//             [body.NewAirDrop.Name]: body.NewAirDrop.Data[address].totalTokens,
 
-        }
-      } else {
+//         }
+//       } else {
     
-        Addresses[address][body.NewAirDrop.Name] = body.NewAirDrop.Data[address].totalTokens;
+//         Addresses[address][body.NewAirDrop.Name] = body.NewAirDrop.Data[address].totalTokens;
 
-      }
+//       }
    
-    }
-}
-var jsonData = JSON.stringify(Addresses);
+//     }
+// }
+// var jsonData = JSON.stringify(Addresses);
 
-fs.writeFile("test.txt", jsonData, function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
+// fs.writeFile("test.txt", jsonData, function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
 
 
 // console.log(Addresses[0x6E9540950B46c35C7C419e57eF6dc6F946B95338])
@@ -152,18 +117,29 @@ fs.writeFile("test.txt", jsonData, function(err) {
 //     }
 //    }
 
-})
+// })
 
 
 app.get('/User/:Address', function(req,res){
-   var test = (req.params.Address);
-   console.log(test);
-    res.json(Addresses[test] )
-   
-    console.log(Addresses[test]);
+  Wallet.findById(req.params.Address, function (err, result) {
+    if (result){
+        res.json(result)
+        
+    }
+    else{
+        res.json("No Address Found")
+    }
+});
+  
 })
 
 app.get('/Projects', function(req,res){
-     res.json(Projects )
-     console.log(Projects);
+    Protocol.find(function (err, result) {
+        if (result){
+            res.json(result)
+        }
+        else{
+            res.json("No Projects Found")
+        }
+    });
  })
