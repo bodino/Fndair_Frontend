fcec1cc73098097b52d8123f8753c833cfd1066a
bodino <a.bifulco23@gmail.com>
2022-06-22 20:31:05 -0700

    priceupdator

---
+++ MoneyStuff/tokenpriceupdator.js
@@ -0,0 +1,46 @@
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const Protocol = require('../Backend/Protocol.js');
+const { findOne, findByIdAndUpdate } = require('../Backend/Protocol.js');
+const uri = 'mongodb+srv://bodo:pkPau37eVc3HHtNX@fndair.6qw8v.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(uri)
+var db = mongoose.connection
+
+
+
+async function tokenUsdPrice() {
+    
+    var food = await Protocol.find()
+    // console.log(food);
+    var idList = [""]
+    var bob = []
+    for (let i = 0; i < food.length; i++) {
+      
+     bob[i] = food[i].toObject()
+        idList[i] = bob[i].geckoId
+        console.log(bob[0].geckoId)
+    }
+    let data = await CoinGeckoClient.coins.markets({
+        ids: idList,
+      });
+
+      console.log(data.data.length)
+    for (let i = 0; i < data.data.length; i++) {
+      for (let j = 0; j < food.length; j++) {
+      
+        if (bob[i].geckoId === data.data[j].id){
+          var ID = bob[i].claimAddress
+          var price = data.data[j].current_price;
+          console.log(price)
+          await Protocol.findByIdAndUpdate( ID, {priceUsd: price})
+         
+        }
+      }
+    }
+
+    console.log("PricesUpdated")
+}
+
+tokenUsdPrice()
