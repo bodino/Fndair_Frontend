3136f3a35385b2d10ca30dd7e3a7bc3acdd5400b
bodino <a.bifulco23@gmail.com>
2022-07-15 23:00:22 -0700

    changed domain

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(
   cors({
-    origin: ['https://funny-vacherin-5815be.netlify.app/'],
+    origin: ['https://funny-vacherin-5815be.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
